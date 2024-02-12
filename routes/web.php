7b22ef26830c18e7b771cc1be2fa903b229a23d4
shero121 <keithsherwin6764@gmail.com>
2024-02-12 15:59:04 +0000

    i think i  have step 1 copmplete

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/customers/new', 'App\Http\Controllers\CustomerController@new');
+Route::post('/customers/create', 'App\Http\Controllers\CustomerController@create')->name('customers.create'); 
 
 Route::resource('members', App\Http\Controllers\memberController::class);
