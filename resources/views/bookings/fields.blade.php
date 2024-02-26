71fbd89583ecacb32a0ade37cde73478bc809d9e
shero121 <keithsherwin6764@gmail.com>
2024-02-26 14:13:54 +0000

    start of week 6

---
+++ resources/views/bookings/fields.blade.php
@@ -0,0 +1,49 @@
+<!-- Bookingdate Field -->
+<div class="form-group col-sm-6">
+    {!! Form::label('bookingdate', 'Bookingdate:') !!}
+    {!! Form::text('bookingdate', null, ['class' => 'form-control','id'=>'bookingdate']) !!}
+</div>
+
+@push('page_scripts')
+    <script type="text/javascript">
+        $('#bookingdate').datetimepicker({
+            format: 'YYYY-MM-DD HH:mm:ss',
+            useCurrent: true,
+            sideBySide: true
+        })
+    </script>
+@endpush
+
+<!-- Starttime Field -->
+<div class="form-group col-sm-6">
+    {!! Form::label('starttime', 'Starttime:') !!}
+    {!! Form::text('starttime', null, ['class' => 'form-control']) !!}
+</div>
+
+<!-- Endtime Field -->
+<div class="form-group col-sm-6">
+    {!! Form::label('endtime', 'Endtime:') !!}
+    {!! Form::text('endtime', null, ['class' => 'form-control']) !!}
+</div>
+
+<!-- Memberid Field -->
+<div class="form-group col-sm-6">
+    <select name="memberid" class="form-control">
+        @foreach($members as $member)
+            <option value="{{$member->id}}">{{$member}}</option>
+        @endforeach
+
+    </select>
+</div>
+
+<!-- Courtid Field -->
+<div class="form-group col-sm-6">
+    {!! Form::label('courtid', 'Courtid:') !!}
+    {!! Form::number('courtid', null, ['class' => 'form-control']) !!}
+</div>
+
+<!-- Fee Field -->
+<div class="form-group col-sm-6">
+    {!! Form::label('fee', 'Fee:') !!}
+    {!! Form::number('fee', null, ['class' => 'form-control']) !!}
+</div>
