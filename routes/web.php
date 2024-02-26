fee202878b1b3f541500ad180d52dedc8206586f
shero121 <keithsherwin6764@gmail.com>
2024-02-26 14:43:54 +0000

    did first part with breeze and login feature worked. registered account and tested

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
 
 
 Route::get('/customers/new', 'App\Http\Controllers\CustomerController@new');
